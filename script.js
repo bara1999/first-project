183fcb63f13cd42f1474824a4e74a5810fe19445
bara1999 <171068@ppu.edu.ps>
2020-07-17 16:19:43 +0300

    https://github.com/bara1999/first-project.git
    Signed-off-by: bara1999 <171068@ppu.edu.ps>

--- script.js
+++ script.js
@@ -1,19 +1,59 @@
  const listcontainer =document.querySelector('[data-lists]')
 const newListForm=document.querySelector('[data-new-list-form]')
 const newListInput=document.querySelector('[data-new-list-input]')
+const deleteListButton = document.querySelector('[data-delete-list-button]')
+const listDisplayContainer=document.querySelector('[ data-list-display-container]')
+const listTilteElement=document.querySelector('[data-list-title]')
+const tasksContainer=document.querySelector('[data-tasks]')
+const  listCountElement=document.querySelector('[data-list-count]')
+const taskTemplete =document.getElementById('task-template')
+const newTaskForm = document.querySelector('[data-new-task-form]')
+const newTaskInput = document.querySelector('[data-new-task-input]')
+const clearCompleteTasksButton = document.querySelector('[data-clear-complete-tasks-button]')
+
+
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'//?
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
 
  let lists=JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY))||[]
  let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY) 
 
+
+
+
  listcontainer.addEventListener('click', e => {
     if (e.target.tagName.toLowerCase() === 'li') {
-      selectedListId = e.target.dataset.listId
+      selectedListId = e.target.dataset.listid
       saveAndRender()
     }
   })
 
+tasksContainer.addEventListener('click' ,e =>{
+    if(e.target.tagName.toLowerCase() === 'input'){
+        
+        const selectedList=lists.find(list => list.id===selectedListId)
+        const selectedTask=selectedList.tasks.find(task => task.id===e.target.id)//render from checkbox.id
+
+        selectedTask.complete=e.target.checked
+        save()
+        renderTaskCount(selectedList)
+        
+    }
+})
+
+clearCompleteTasksButton.addEventListener('click',e=>{
+   
+    const selectedList=lists.find(list => list.id===selectedListId)
+    selectedList.tasks=selectedList.tasks.filter(task => !task.complete)
+    saveAndRender()
+})
+  deleteListButton.addEventListener('click', e => {
+    console.log(lists)
+    lists = lists.filter(list => list.id !== selectedListId)
+    selectedListId = null
+    saveAndRender()
+    
+})
  newListForm.addEventListener('submit', e=>{
      e.preventDefault()
      const listName=newListInput.value
@@ -24,8 +64,24 @@ const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
     
      saveAndRender()
  })
+
+ newTaskForm.addEventListener('submit', e=>{
+    e.preventDefault()
+    const taskName=newTaskInput.value
+    if(taskName==null || taskName==='') return
+    const task=createTask(taskName)
+    newTaskInput.value=null;
+    const selectedList=lists.find(list => list.id === selectedListId)
+    selectedList.tasks.push(task)
+   
+    saveAndRender()
+})
+
+  function  createTask(name){
+    return { id: Date.now().toString(), name: name, complete:false }
+  }
  function createList(name){
-     return { id: Date.now().toString(), name: name, tasks: [] }
+     return { id: Date.now().toString(), name: name, tasks: [ ] }
  }
  function saveAndRender(){
      save()
@@ -35,20 +91,76 @@ const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
     localStorage.setItem(LOCAL_STORAGE_LIST_KEY,JSON.stringify(lists))
     localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
 }
+
+
  function render(){
      clearElement(listcontainer)
-     lists.forEach(list => {
-         const listElement =document.createElement('li')
-         listElement.dataset.listid=list.id;
-         listElement.classList.add("list-name")
-         listElement.innerText=list.name
-         if(list.id==selectedListId){
-             listElement.classList.add("active-list")
-         }
-         listcontainer.appendChild(listElement)
+     renderlist()
+     const selectedList=lists.find(list=> list.id==selectedListId )
+     if(selectedListId==null)
+    {
+        listDisplayContainer.style.display = 'none'
+    }
+    else
+    { 
+         listDisplayContainer.style.display='' //شو كان موجود قبل
+         listTilteElement.innerText=selectedList.name
+         renderTaskCount(selectedList)
+         clearElement(tasksContainer)
+         renderTasks(selectedList)
 
+    }
+ }
+
+ function renderTasks(selectedList){
+    selectedList.tasks.forEach(task => {
+       //The htmlFor property sets or returns the value of the for attribute of a label.
          
+         const taskElement = document.importNode(taskTemplete.content, true)
+         const checkbox=taskElement.querySelector('input')
+         
+         checkbox.id=task.id
+         checkbox.checked=task.complete
+         const label= taskElement.querySelector('label')
+         
+    
+         label.htmlFor=task.id
+         
+     
+         label.append(task.name)
+         tasksContainer.appendChild(taskElement)
+    
+   
+  
+         
+
+
+
      })
+
+ }
+
+ function renderTaskCount(selectedList) {
+  
+     const incompleteTasksCount=selectedList.tasks.filter(task =>!task.complete).length
+     console.log(selectedList.tasks.filter(task =>!task.complete))
+     const taskString= incompleteTasksCount===1? "task": "tasks"
+     listCountElement.innerText= `${incompleteTasksCount} ${taskString} remaining`
+ }
+
+ function renderlist(){
+    lists.forEach(list => {
+        const listElement =document.createElement('li')
+        listElement.dataset.listid=list.id;
+        listElement.classList.add("list-name")
+        listElement.innerText=list.name
+        if(list.id==selectedListId){
+            listElement.classList.add("active-list")
+        }
+        listcontainer.appendChild(listElement)
+
+        
+    })
  }
 function  clearElement(element){
     while(element.firstChild){
